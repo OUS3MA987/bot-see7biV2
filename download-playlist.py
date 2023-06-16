7bf3653e3b76fdc724eab7d02a417cec70091e9a
Oussema Trabelsi <oussema99trabelsi@gmail.com>
2023-06-16 17:23:22 +0100

    script to download video / playlist from youtube

---
+++ download-playlist.py
@@ -0,0 +1,11 @@
+import yt_dlp
+ytdl_format_options = {
+    'format': 'bestaudio/best',
+    'outtmpl': 'downloads/%(title)s.mp3',
+    'ignoreerrors': True,
+    'quiet': True,
+}
+ytdl = yt_dlp.YoutubeDL(ytdl_format_options)
+url = input("Youtube Video/Playlist link : ")
+ytdl.extract_info(url, download=True)
+
